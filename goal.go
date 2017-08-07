cc752b810ca4c0bdbc50c24a19aa11c29c0406c4
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2017-08-06 22:44:49 -0700

    Add goal type

---
+++ goal.go
@@ -0,0 +1,18 @@
+package patreon
+
+import "time"
+
+// Goal represents a Patreon's goal
+type Goal struct {
+	Type       string `json:"type"`
+	Id         int    `json:"id"`
+	Attributes struct {
+		Amount              int       `json:"amount"`
+		AmountCents         int       `json:"amount_cents"`
+		CompletedPercentage int       `json:"completed_percentage"`
+		CreatedAt           time.Time `json:"created_at"`
+		ReachedAt           time.Time `json:"reached_at"`
+		Title               string    `json:"title"`
+		Description         string    `json:"description"`
+	} `json:"attributes"`
+}
