e249e0657e60006fe9eeb4075046be4b99a2dfe7
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2017-08-08 10:00:50 -0700

    Add refresh token example

--- example_test.go
+++ example_test.go
@@ -1,8 +1,10 @@
 package patreon
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"time"
 
 	"golang.org/x/oauth2"
 )
@@ -72,4 +74,33 @@ func Example_fetchPatronsAndPledges() {
 	}
 
 	fmt.Print("Done!")
+}
+
+// Automatically refresh token
+func Example_refreshToken() {
+	config := oauth2.Config{
+		ClientID:     "<client_id>",
+		ClientSecret: "<client_secret>",
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  AuthorizationURL,
+			TokenURL: AccessTokenURL,
+		},
+		Scopes: []string{"users", "pledges-to-me", "my-campaign"},
+	}
+
+	token := oauth2.Token{
+		RefreshToken: "<current_refresh_token>",
+		// Must be non-nil, otherwise token will not be expired
+		Expiry: time.Now().Add(-24 * time.Hour),
+	}
+
+	tc := config.Client(context.Background(), &token)
+
+	client := NewClient(tc)
+	_, err := client.FetchUser()
+	if err != nil {
+		panic(err)
+	}
+
+	print("OK")
 }
