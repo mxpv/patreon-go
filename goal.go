3f9284a2dc633da9e9c5bed1e76ee74118ca269b
abspen1 <abspencer2097@gmail.com>
2022-09-01 07:11:45 -0700

    Update goal for V2

--- goal.go
+++ goal.go
@@ -1,16 +1,24 @@
 package patreon
 
-// Goal represents a Patreon's goal.
+// GoalAttributes is all fields in the Goal Attributes struct
+var GoalAttributes = []string{
+	"AmountCents", "CompletedPercentage", "CreatedAt",
+	"Description", "ReachedAt", "Title",
+}
+
+// Goal is the funding goal in USD set by a creator on a campaign.
 type Goal struct {
 	Type       string `json:"type"`
 	ID         string `json:"id"`
 	Attributes struct {
-		Amount              int      `json:"amount"`
 		AmountCents         int      `json:"amount_cents"`
 		CompletedPercentage int      `json:"completed_percentage"`
 		CreatedAt           NullTime `json:"created_at"`
+		Description         string   `json:"description"`
 		ReachedAt           NullTime `json:"reached_at"`
 		Title               string   `json:"title"`
-		Description         string   `json:"description"`
 	} `json:"attributes"`
+	Relationships struct {
+		Campaign *CampaignRelationship `json:"campaign,omitempty"`
+	} `json:"relationships"`
 }
